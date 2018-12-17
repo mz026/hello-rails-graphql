55b73bc0dfbe0214844ad8fa8176bf0e2ba7eb5f
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2018-12-17 16:44:32 +0800

    add comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_17_084209) do
+ActiveRecord::Schema.define(version: 2018_12_17_084352) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
