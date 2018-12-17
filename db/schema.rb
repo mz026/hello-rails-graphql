16ce502a98498ae4caaaa42df74ffe20275d016c
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2018-12-17 16:43:47 +0800

    add post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_17_043305) do
+ActiveRecord::Schema.define(version: 2018_12_17_084209) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
