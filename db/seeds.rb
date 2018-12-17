e9bfbc8369829079fc55e63674fa1b65c0a8c45c
Yang-Hsing Lin <yanghsing.lin@gmail.com>
2018-12-17 16:49:09 +0800

    add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = (1..10).map do |i|
+  User.create!({
+    name: "User #{i}",
+    age: 20 + i,
+    email: "jack-#{i}@test.com",
+    username: "username-#{i}"
+  })
+end
+
+users.each do |u|
+  3.times do |i|
+    idx = i + 1
+    Post.create!({
+      title: "Post ID #{u.id}-#{idx}",
+      user_id: u.id
+    })
+  end
+end
+
+Post.all.each do |po|
+  3.times do |i|
+    Comment.create!({
+      post_id: po.id,
+      content: "comment content -- #{po.id}-#{i}"
+    })
+  end
+end
